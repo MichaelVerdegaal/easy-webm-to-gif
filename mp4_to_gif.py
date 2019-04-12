5470a0c0aac3aaeca24a4d5b22151a9c91c3a211
Michael V <michael.verdegaal@hva.nl>
2019-04-12 13:07:45 +0200

    Add mp4 alternative

---
+++ mp4_to_gif.py
@@ -0,0 +1,14 @@
+import os
+from moviepy.editor import VideoFileClip
+
+for file in os.listdir(os.getcwd()):
+    if file.endswith(".mp4"):
+        print(f'Converting {file}')
+        try:
+            video = VideoFileClip(file)
+            new_file_name = f"{file.split('.webm')[0]}.gif"
+            video.write_gif(new_file_name)
+            print(f'Successfully converted {file} to {new_file_name}\n')
+        except Exception as e:
+            print(f'Error converting {file}: {e}')
+input('Process finished, press enter to exit')
